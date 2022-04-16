f606f4712965bc4a3e4b65faf5ffe38b922b845a
FuryCydonian <el.fury.oos@gmail.com>
2022-04-16 20:32:25 +0300

    add custom login page, start Admin and User pages with bootstrap, finished login page design

---
+++ src/main/java/com/fury_cydonian/spring_boot/controller/LoginController.java
@@ -0,0 +1,23 @@
+package com.fury_cydonian.spring_boot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String getLogin(@RequestParam(value = "error", required = false) String error,
+                           @RequestParam(value = "logout", required = false) String logout,
+                           Model model) {
+        model.addAttribute("error", error != null);
+        model.addAttribute("logout", error != null);
+        return "login";
+    }
+
+//    @PostMapping
+//    public
+}
--- src/main/java/com/fury_cydonian/spring_boot/model/User.java
+++ src/main/java/com/fury_cydonian/spring_boot/model/User.java
@@ -42,7 +42,6 @@ public class User implements UserDetails {
     }
 
     public User() {
-
     }
 
     public long getId() {

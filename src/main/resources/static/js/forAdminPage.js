14ba5dc07555111368cd5612147b0d57f4681243
FuryCydonian <ella.bir@hotmail.com>
2022-05-01 03:47:55 +0300

    finish js file for post user's data from modal window to controller

--- src/main/resources/static/js/forAdminPage.js
+++ src/main/resources/static/js/forAdminPage.js
@@ -1,73 +1,110 @@
-// import * as service from './static/js/fetchService';
-
 $(function () {
 
     console.log('HELLO FROM JS FILE!!!!!!!!!!!!!!!!!!!!!')
 
-    editModal()
+    onClickTableButton()
 });
 
 const userFetchService = {
-    head: {
+    headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
-        'Referer': null
+        // 'Referer': null
     },
     findAllUsers: async () => await fetch('api/users'),
     findOneUserById: async (id) => await fetch(`api/users/${id}`),
     addNewUser: async (user) => await fetch('api/users', {
         method: 'POST',
-        headers: userFetchService.head,
+        headers: userFetchService.headers,
         body: JSON.stringify(user)
     }),
     updateUser: async (user, id) => await fetch(`api/users/${id}`, {
         method: 'PUT',
-        headers: userFetchService.head,
+        headers: userFetchService.headers,
         body: JSON.stringify(user)
     }),
     deleteUser: async (id) => await fetch(`api/users/${id}`, {
         method: 'DELETE',
-        headers: userFetchService.head
+        headers: userFetchService.headers
     }),
     findAllRoles: async () => await fetch('api/roles'),
 }
 
-const editModal = () => {
-    $('edit-user-modal').modal({
-        show: false
-    }).on('show.bs.modal', (e) => {
-        let button = $(e.relatedTarget)
-        let id = button.data('userId')
-        editUser($(this), id)
+const onClickTableButton = () => {
+    $("#mainTableWithUsers").find('button').on('click', (event) => {
+        let targetButton = $(event.target);
+        console.log('target button: ' + targetButton)
+
+        let userId = targetButton.data('id')
+        console.log('user ID: ' + userId)
+
+        let action = targetButton.data('action')
+        console.log('action: ' + action)
+
+        if (action === 'edit') {
+            console.log('IF EDIT!!!!!!!!!!!!!!!')
+            editUser(userId)
+        }
     })
 }
 
-const editUser = (modal, id) => {
-    modal.find('#editFormBtn').on('click', async (event) => {
+const editUser = (id) => {
+    let modal = $(`#editUserModal${id}`)
+    modal.find(`#editFormBtn${id}`).on('click', async (event) => {
+        console.log('EDIT BUTTON CLICKED')
+
         let id = modal.find('#editID').val().trim()
+        console.log('USER ID FROM MODAL: ' + id)
+
         let firstName = modal.find('#editFirstName').val().trim()
-        let email = modal.find('#editEmail').val().trim()
-        let password = modal.find('#editPassword').val().trim()
+        let email = modal.find(`#editEmail${id}`).val().trim()
+        let password = modal.find(`#editPassword${id}`).val().trim()
 
-        let rolesFromForm = modal.find('#editRoles').val().trim()
+        if (email === '' || password === '') {
+            alert('It seems You forgot email or password')
+            return
+        }
 
-        // let userRoles = []
+        if (!isEmail(email)) {
+            alert('Wrong email format')
+            return
+        }
+
+        let rolesIDFromForm = modal.find('#editRoles').val();
+        console.log('ROLES_ID: ' + rolesIDFromForm)
+
+        let userRoles = []
+
+        let allRolesFromDB = await (await userFetchService.findAllRoles()).json()
+        console.log(allRolesFromDB)
+
+        allRolesFromDB.forEach(roleFromDB => {
+            rolesIDFromForm.forEach(roleID => roleID == roleFromDB.id ? userRoles.push(roleFromDB) : null)
+        })
 
         let data = {
             id: id,
             firstName: firstName,
             email: email,
-            password: email,
-            roles: rolesFromForm
+            password: password,
+            roles: userRoles
         }
 
         console.log(data)
 
-        let response = await userFetchService.updateUser(data.id)
+        let response = await userFetchService.updateUser(data, id)
+
         if (response.ok) {
-            alert(`${email} successfully updated`)
             modal.modal('hide')
+            location.reload();
+        } else {
+            alert(`response status: ${response.status}, 
+            ${response.body.toString()}`)
         }
-        alert(`response status: ${response.status}, /n ${response.body}`)
     })
+}
+
+const isEmail = (email) => {
+    let regExp = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/
+    return regExp.test(email)
 }
